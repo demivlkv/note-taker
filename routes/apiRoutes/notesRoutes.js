a1124990103e1729fbda1a40937cf4964aa4e526
Demi Hayashi <demi.h@me.com>
2022-08-17 08:19:14 -0700

    add createNote function

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+const { createNote } = require('../../lib/notes');
+const { db } = require('../../db/db.json');
+
+// add route
+router.get('/notes', (req, res) => {
+    let results = notesArray;
+
+    res.json(results);
+})
+
 module.exports = router;
