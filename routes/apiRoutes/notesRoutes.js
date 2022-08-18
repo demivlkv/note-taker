1bb8ee8c3908685ad75a10b0db8df24dc92182e7
Demi Hayashi <demi.h@me.com>
2022-08-18 16:03:56 -0700

    added get & post methods in routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,18 +1,26 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const { createNote } = require('../../lib/notes');
 const db = require('../../db/db.json');
 
-// add route
+// get notes
 router.get('/notes', (req, res) => {
-    console.log(db);
+    // console.log(db);
     res.json(db);
 });
 
+// post notes
 router.post('/notes', (req, res) => {
     let note = req.body;
-
-    
+    note.id = uuidv4();
+    db.push(note);
+    fs.writeFile('./db/db.json', JSON.stringify(db), function(err){
+        if (err) throw err
+        res.json(note);
+    })
 })
 
 module.exports = router;
