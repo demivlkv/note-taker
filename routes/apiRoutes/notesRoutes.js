6865764538b2f67286dd09d7f311ddac74f9d941
Demi Hayashi <demi.h@me.com>
2022-08-18 15:30:53 -0700

    edit files

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 
 const { createNote } = require('../../lib/notes');
-const { db } = require('../../db/db.json');
+const db = require('../../db/db.json');
 
 // add route
 router.get('/notes', (req, res) => {
-    let results = notesArray;
+    console.log(db);
+    res.json(db);
+});
 
-    res.json(results);
+router.post('/notes', (req, res) => {
+    let note = req.body;
+
+    
 })
 
 module.exports = router;
