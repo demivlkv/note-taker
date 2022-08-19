395973a674b8e8d6f912228791a828d34e6b6b54
Demi Hayashi <demi.h@me.com>
2022-08-18 22:34:27 -0700

    add DELETE request

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,25 +2,38 @@ const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-
-const { createNote } = require('../../lib/notes');
 const db = require('../../db/db.json');
 
-// get notes
+// GET notes from db.json
 router.get('/notes', (req, res) => {
     // console.log(db);
     res.json(db);
 });
 
-// post notes
+// POST request to add a note to array
 router.post('/notes', (req, res) => {
     let note = req.body;
+    // set unique id to notes
     note.id = uuidv4();
     db.push(note);
-    fs.writeFile('./db/db.json', JSON.stringify(db), function(err){
-        if (err) throw err
+
+    // write note to db.json & re-write db.json
+    fs.writeFile('./db/db.json', JSON.stringify(db, null, 2), function(err) {
+        if (err) throw err;
         res.json(note);
+        console.log('Note saved!');
     })
-})
+});
+
+// DELETE note according by ID
+router.delete('/notes/:id', (req, res) => {
+    const { id } = req.params;
+    let notesIndex = db.findIndex(note => note.id == id);
+
+    db.splice(notesIndex, 1);
+
+    res.json(db);
+    console.log('Note deleted!');
+});
 
 module.exports = router;
